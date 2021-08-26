c05da82c9d0b8ee8964eae8c7f009005c5f10e63
Zdeněk Pašek <me@zdenekpasek.cz>
2021-08-26 15:41:36 +0200

    tailwindcss init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1 class="text-8xl font-black text-yellow-400">Hello World</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
